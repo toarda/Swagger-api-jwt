e1f1ba6d9b2836690470560402b5e4c3d19f572d
toarda <tor.archil.dav@gmail.com>
2023-06-04 14:37:15 +0600

    full project

---
+++ app/Http/Controllers/Swagger/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(
+ *      title="My Doc API",
+ *      version="1.0"
+ * ),
+ * 
+ * @OA\PathItem(
+ *      path="/api/"
+ * ),
+ * 
+ * @OA\Components(
+ *      @OA\SecurityScheme(
+ *          securityScheme="bearerAuth",
+ *          type="http",
+ *          scheme="bearer"
+ *      )
+ * ),
+ * 
+ * 
+ */
+
+class MainController extends Controller
+{
+    //
+}
